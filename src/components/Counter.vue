59858ba7a7d4d98328159a343774af161ed83865
DuySE <duynse@gmail.com>
2022-05-31 23:16:00 +0700

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>My name is {{ toUpperCase }}</p>
+    <p>{{ count }}</p>
+    <input type="number" name="value" v-model.number="value"/>
+    <button @click="increase(value)">+</button>
+    <button @click="decrease(value)">-</button>
+  </div>
+</template>
+<script>
+import { mapMutations, mapState } from 'vuex'
+export default {
+  data () {
+    return {
+      name: 'Duy',
+      value: 0
+    }
+  },
+  computed: {
+    toUpperCase: function () {
+      return this.name.toUpperCase()
+    },
+    ...mapState(['count'])
+  },
+  methods: {
+    ...mapMutations(['increase', 'decrease'])
+  }
+}
+</script>
